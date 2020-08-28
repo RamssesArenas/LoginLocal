5869dc9231585a676a5fc893c12c8729b4f8cea8
RamssesArenas <l15200119@pachuca.tecnm.mx>
2020-08-27 23:31:33 -0500

    Login Localstorage

---
+++ docs/precache-manifest.1a8d9ac3ca388f13cd5b16b1409a4f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fc61c11d6ec73058681f87b9b93f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3f17dfccd21b4eb190e",
+    "url": "/static/css/main.752eb498.chunk.css"
+  },
+  {
+    "revision": "6aeaf264264df13a36db",
+    "url": "/static/js/2.144179cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.144179cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f17dfccd21b4eb190e",
+    "url": "/static/js/main.5f923f6c.chunk.js"
+  },
+  {
+    "revision": "eebcbf0c54856a2ff127",
+    "url": "/static/js/runtime-main.236883d7.js"
+  },
+  {
+    "revision": "f034e66a3fb637daea4f9f97be5912ad",
+    "url": "/static/media/img_001.f034e66a.jpg"
+  }
+]);
